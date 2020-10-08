6f87ce8dee40a7a5b5108357fef0d8c4f0bd2b3f
Luis Lozano <lslozano11@gmail.com>
2020-10-08 13:32:25 -0500

    Require modules and server listen

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis velit eu metus luctus dapibus. Donec et ligula ante. Curabitur eget massa ac felis egestas dictum. Cras cursus aliquam tellus eu sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus faucibus risus id magna efficitur lobortis. Morbi malesuada augue at massa luctus, eu dictum ante ultrices. Suspendisse at purus nisi. Duis vestibulum sagittis augue vitae ultricies. Fusce consequat consequat lectus, ut rutrum nibh fringilla imperdiet. Integer egestas mi at mauris fringilla, vitae gravida nisl egestas.";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis velit eu metus luctus dapibus. Donec et ligula ante. Curabitur eget massa ac felis egestas dictum. Cras cursus aliquam tellus eu sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus faucibus risus id magna efficitur lobortis. Morbi malesuada augue at massa luctus, eu dictum ante ultrices. Suspendisse at purus nisi. Duis vestibulum sagittis augue vitae ultricies. Fusce consequat consequat lectus, ut rutrum nibh fringilla imperdiet. Integer egestas mi at mauris fringilla, vitae gravida nisl egestas.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis velit eu metus luctus dapibus. Donec et ligula ante. Curabitur eget massa ac felis egestas dictum. Cras cursus aliquam tellus eu sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus faucibus risus id magna efficitur lobortis. Morbi malesuada augue at massa luctus, eu dictum ante ultrices. Suspendisse at purus nisi. Duis vestibulum sagittis augue vitae ultricies. Fusce consequat consequat lectus, ut rutrum nibh fringilla imperdiet. Integer egestas mi at mauris fringilla, vitae gravida nisl egestas.";
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
